53bfaf1613a40efe92966b1a923e09a45e7b00f2
Vivian Gichure <viviangichure@gmail.com>
2025-09-14 12:33:07 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,25 +1,18 @@
-# server/app.py
-
 from flask import Flask
 from flask_migrate import Migrate
-
 from models import db
 
-# create a Flask application instance
 app = Flask(__name__)
 
-# configure the database connection to the local file app.db
+# Configure SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# configure flag to disable modification tracking and use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create a Migrate object to manage schema modifications
+# Initialize Flask-Migrate
 migrate = Migrate(app, db)
 
-# initialize the Flask application to use the database
+# Initialize Flask-SQLAlchemy
 db.init_app(app)
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,18 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# contains definitions of tables and associated schema constructs
+# Metadata object for table info
 metadata = MetaData()
 
-# create the Flask SQLAlchemy extension
+# SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
 
-# define a model class by inheriting from db.Model.
-
-
+# Define Pet model
 class Pet(db.Model):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Pet {self.id}, {self.name}, {self.species}>'
